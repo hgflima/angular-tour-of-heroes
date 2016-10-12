dc824e1fdfdf471ff011d10acb5e2b315c9eb032
Henrique Lima <hgflima@gmail.com>
2016-10-12 10:05:41 -0300

    adding dashboard component

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -6,18 +6,20 @@ import { HeroService }  from './hero.service';
 @Component({
   moduleId: module.id,
   selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
+  templateUrl: 'dashboard.component.html'
 })
 
 export class DashboardComponent implements OnInit {
 
+  heroes: Hero[] = []
 
   constructor(private heroService: HeroService) {
 
   }
 
   ngOnInit(): void {
-
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1,5));
   }
 
   gotoDetail(hero: Hero): void {
